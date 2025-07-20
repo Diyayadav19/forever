c66b27c977ff8af956e30d2ebb04156978939cbe
diya yadav <diyayadav1905@gmail.com>
2025-07-20 18:17:59 +0530

    first commit

--- backened/controllers/ordercontroller.js
+++ backened/controllers/ordercontroller.js
@@ -4,9 +4,6 @@ import Stripe from 'stripe'
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-
-console.log('Stripe Key:', process.env.STRIPE_SECRET_KEY);
 const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const currency='USD'
